222145d9f1bc176a6d55eb0ef0f8346882376f44
HM <hello@hazella.co>
2017-01-11 10:13:38 -0600

    Prevent Dups

--- app/src/main/java/com/hm17/dailyaphorisms/Aphorisms.java
+++ app/src/main/java/com/hm17/dailyaphorisms/Aphorisms.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,13 +13,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Aphorisms extends AppCompatActivity {
-    int dailyCount = 0;
+    private int dailyCount = 0;
 
     // Users are limited to 3 quotes a day.
     // How to restrict users from resetting dailycount when app restarts?
-    int hardLimit = 3;
+    private int hardLimit = 2;
+
+    private final static String ERROR_LIMIT_MSG = "Sorry! You've reached your limit for today!";
 
     Map<Integer, String> dictionary = new HashMap<>();
+    Map<Integer, Integer> cache = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +30,55 @@ public class Aphorisms extends AppCompatActivity {
         setContentView(R.layout.activity_aphorisms);
 
         populateDictionary();
+        setText();
+
     }
 
     public void nextAphorism(View view) {
+        setText();
+        dailyCount++;
+    }
+
+    private void setText(){
         String text;
 
         if(dailyCount >= hardLimit){
-            text = "Sorry! You've reached your limit for today!";
+            TextView textView = (TextView) findViewById(R.id.textInfo);
+            textView.setText(ERROR_LIMIT_MSG);
+            textView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+            text = "";
         }
         else {
-            // Need to keep track of duplicate numbers so hard limit is not reached with dups
-            int random = (int )(Math.random() * dictionary.size() + 1);
-            text = dictionary.get(random);
+            int random = getQuoteId();
+            int numAttempts = 0;
+            Boolean onlyDupsLeft = false;
+
+            // Check cache for dups up to 3 times
+            while(cache.containsKey(random)){
+                random = getQuoteId();
+                if(numAttempts >= hardLimit){
+                    onlyDupsLeft = true;
+                    break;
+                }
+            }
+            if(onlyDupsLeft){
+                text = ERROR_LIMIT_MSG;
+            }
+            else {
+                text = dictionary.get(random);
+                cache.put(random, 1);
+            }
+
         }
         TextView textView = (TextView) findViewById(R.id.textDisplay);
         textView.setText(text);
-        dailyCount++;
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
+        //textView.setWidth();
+    }
+
+    private int getQuoteId(){
+        return (int )(Math.random() * dictionary.size());
     }
 
     private void populateDictionary(){
@@ -51,6 +88,7 @@ public class Aphorisms extends AppCompatActivity {
         dictionary.put(3, "When you are stuck in a place where you feel you are not good enough, " +
                 "remember that what is right with you is always more than what is wrong with you.");
         dictionary.put(4, "Anything is possibleeeeee!");
+        dictionary.put(5, "Your vibe attracts your tribe.");
     }
 
 }
