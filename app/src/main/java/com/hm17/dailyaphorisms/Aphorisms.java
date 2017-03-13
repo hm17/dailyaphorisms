d6fa80bc436254c6cec4d145493437e34d37fcb5
HM <hello@hazella.co>
2017-03-12 19:46:56 -0500

    Updates

--- app/src/main/java/com/hm17/dailyaphorisms/Aphorisms.java
+++ app/src/main/java/com/hm17/dailyaphorisms/Aphorisms.java
@@ -1,10 +1,9 @@
 package com.hm17.dailyaphorisms;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
@@ -21,8 +20,12 @@ public class Aphorisms extends AppCompatActivity {
 
     private final static String ERROR_LIMIT_MSG = "Sorry! You've reached your limit for today!";
 
+    private final static String COLOR_PINK = "FFCCFF";
+    private final static String COLOR_TEAL = "1FC4BA";
+
     Map<Integer, String> dictionary = new HashMap<>();
     Map<Integer, Integer> cache = new HashMap<>();
+    Map<Integer, String> colorCache = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +35,35 @@ public class Aphorisms extends AppCompatActivity {
         populateDictionary();
         setText();
 
+        buildColorCache();
+
     }
 
     public void nextAphorism(View view) {
+        changeBackgroundColor();
         setText();
         dailyCount++;
     }
+
+    private void changeBackgroundColor(){
+        View someView = findViewById(R.id.OuterRelativeLayout);
+
+        // Find the root view
+        View root = someView.getRootView();
+
+        // Pick a random color
+        //int color = getColor(getRandomNumber());
+
+        // Set the color
+        root.setBackgroundColor(Color.parseColor(COLOR_PINK));
+    }
+
+    /**
+    private int getColor(int colorId){
+        // get color from cache
+        return colorCache.get(colorId);
+    }
+    **/
 
     private void setText(){
         String text;
@@ -45,7 +71,11 @@ public class Aphorisms extends AppCompatActivity {
         if(dailyCount >= hardLimit){
             TextView textView = (TextView) findViewById(R.id.textInfo);
             textView.setText(ERROR_LIMIT_MSG);
+
             textView.setGravity(Gravity.CENTER_HORIZONTAL);
+
+
+
 
             text = "";
         }
@@ -74,12 +104,19 @@ public class Aphorisms extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.textDisplay);
         textView.setText(text);
         textView.setGravity(Gravity.CENTER_HORIZONTAL);
-        //textView.setWidth();
+
+        // Set width to be parent
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindow().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        textView.getLayoutParams().width = width;
     }
 
     private int getQuoteId(){
         return (int )(Math.random() * dictionary.size());
     }
+
+    private int getRandomNumber() {return (int )(Math.random() * dictionary.size());}
 
     private void populateDictionary(){
         dictionary.put(0, "I crave a love so deep the ocean would be jealous.");
@@ -89,6 +126,12 @@ public class Aphorisms extends AppCompatActivity {
                 "remember that what is right with you is always more than what is wrong with you.");
         dictionary.put(4, "Anything is possibleeeeee!");
         dictionary.put(5, "Your vibe attracts your tribe.");
+    }
+
+    // TODO: Get colors from DB configuration
+    private void buildColorCache(){
+        colorCache.put(0, COLOR_PINK);
+        colorCache.put(1, COLOR_TEAL);
     }
 
 }
